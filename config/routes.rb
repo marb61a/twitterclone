93667a1c6a16b8a76b3b153a3097a1879b1fe980
marb <marb61a@gmail.com>
2015-07-02 13:43:59 +0000

    adding more partial views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'help' => 'static_pages/help'
+  get 'about' => 'static_pages/about'
+  get 'contact' => 'static_pages/contact'
+  get 'signup' => 'users#new'
   
 
   resources :users do
@@ -13,8 +14,8 @@ Rails.application.routes.draw do
   end
   
   resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   
 end

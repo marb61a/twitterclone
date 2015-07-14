33c006e8fed5bb18962a06be2272774b848b1331
marb <marb61a@gmail.com>
2015-07-14 13:46:52 +0000

    added migration and password reset controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709141551) do
+ActiveRecord::Schema.define(version: 20150714134551) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20150709141551) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.boolean  "admin",             default: false
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

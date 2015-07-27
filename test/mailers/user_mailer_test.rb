056adcb3598f8ed265695f8f0485de68f6bb7f5d
marb <marb61a@gmail.com>
2015-07-27 13:40:55 +0000

    updated micropost integration test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,5 +1,11 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+    test "account activation" do
+        
+    end
     
+    test "password reset" do
+        
+    end
 end
